3e5a198f5dacb4e2f13991e8b4a0518123170d52
Fabian <fabian@eb1tr.com>
2022-07-25 19:03:12 +0200

    BETA

--- svc/hostcmd/app.py
+++ svc/hostcmd/app.py
@@ -17,9 +17,8 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     dato = msg.payload.decode('utf-8')
-    print(dato)
-    if dato == "shutdown -r now":
-        os.system("reboot")
+    if dato == "reboot":
+        os.system("shutdown -r now")
     elif dato == "poweroff":
         os.system("shutdown -h now")
 
