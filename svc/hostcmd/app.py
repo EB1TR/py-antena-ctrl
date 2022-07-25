c0471849e4b46445bdb48f0739b94c23b23f41af
Fabian <fabian@eb1tr.com>
2022-07-25 18:19:29 +0200

    TEST

--- svc/hostcmd/app.py
+++ svc/hostcmd/app.py
@@ -2,13 +2,18 @@ __author__ = 'EB1TR'
 
 import paho.mqtt.client as mqtt
 import os
+import time
 
 MQTT_HOST = "127.0.0.1"
 MQTT_PORT = 1883
 
+flag_connected = 0
+
 
 def on_connect(client, userdata, flags, rc):
+    global flag_connected
     print("Conectado a MQTT")
+    flag_connected = 1
     client.subscribe([
         ("hostcmd", 0)
     ])
@@ -23,7 +28,14 @@ def on_message(client, userdata, msg):
 
 
 mqtt_client = mqtt.Client()
-mqtt_client.connect(MQTT_HOST, MQTT_PORT, 600)
+
+while flag_connected == 0:
+    try:
+        mqtt_client.connect(MQTT_HOST, MQTT_PORT, 600)
+    except:
+        print(time.time(), "Servidor MQTT no disponible...")
+        time.sleep(3)
+
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 mqtt_client.loop_forever()
