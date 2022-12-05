fd71732b0618656cdd0aa1669ca3c2d4f0ebdbac
Fabian <fabian@eb1tr.com>
2022-12-05 19:01:10 +0100

    REFACTOR: Limpieza

--- ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nuevaip="$(hostname -I | awk '{ print $1}')"
-cd $PATHCTRL/static
-sed -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}'/"$nuevaip"/g mqtt.js > mqtt2.js
-mv mqtt2.js mqtt.js
-
-sed -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}'/"$nuevaip"/g config.js > config2.js
-mv config2.js config.js
